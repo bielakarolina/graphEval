1241e0c196d1ef2e86f8122be72afc54aead6d81
Karolina Biela <bielakarolina77@gmail.com>
2020-06-03 22:17:54 +0200

    parser for libraries

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,17 +7,25 @@ res.send("API is working properly");
 
 
 router.get("/data",async function(req, res, next) {
-    const test = require("../../src/dataBase/test");
-
-    const parsedData = require("../../src/parsers/parserNeo4j/parseDataForVis");
-    console.log("duuupa");
-    let dataStackOverflow = await test.loadStackOverflowData();
-    console.log(dataStackOverflow);
-
-    let data = await parsedData.parseNeo4jDataForVis();
-    //console.log(dataStackOverflow);
-   // let cleanNeo4JDataBase = await test.cleanNeo4JDataBase();
-    res.send({data: data});
+    ///:libName/:dataSet
+    //let currentLib = req.params.libName;
+    // if(currentLib === "Vis") {
+    //     const parsedData = require("../../src/parsers/parserNeo4j/parseDataForVis");
+    //     let data = await parsedData.parseNeo4jDataForVis();
+    //     res.send({data: data});
+    // }
+    // else if(currentLib === "D3"){
+    //     const parsedData = require("../../src/parsers/parserNeo4j/parseDataForVis");
+    //     let data = await parsedData.parseNeo4jDataForD3();
+    //     res.send({data: data});
+    // }
+    // else if(currentLib === "Cytoscape"){
+    console.log("duuuuuuuupa");
+        const parsedData = require("../../src/parsers/parserNeo4j/parseDataForVis");
+        let data = await parsedData.parseNeo4jDataForCytoscape();
+        console.log(data);
+        res.send({elements: data});
+  //  }
 });
 
 router.get("/dataD3",async function(req, res, next) {
