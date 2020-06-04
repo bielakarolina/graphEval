12ec13f0e7a24bcea1f708fdc476048cd06d1ef9
basiachra <basia.chra@gmail.com>
2020-06-04 23:24:49 +0200

    cytoscope

--- src/vizLibraries/CytoscapeReact.js
+++ src/vizLibraries/CytoscapeReact.js
@@ -1,12 +1,16 @@
 import React, {Component} from "react";
 import CytoscapeComponent from 'react-cytoscapejs';
+import COSEBilkent from 'cytoscape-cose-bilkent';
+import Cytoscape from 'cytoscape';
 
+
+Cytoscape.use(COSEBilkent);
 export default class CytoscapeReact extends Component {
     constructor(props) {
         super(props);
         this.state ={
             layout: {
-                name: 'random'//'grid' //random, circle, concentric
+                name: 'cose-bilkent'//'grid' //random, circle, concentric
             },
             elements : this.props.graph.elements
 
@@ -16,9 +20,27 @@ export default class CytoscapeReact extends Component {
     componentDidMount(){
         const {elements} = this.props.graph;
 
+        console.log(elements);
+
         this.setState({elements:elements});
     }
     render(){
-        return <CytoscapeComponent elements={this.state.elements} style={{width: '80vw', height: '100vh'}} layout={this.state.layout}/>;
+        return <CytoscapeComponent elements={this.state.elements} stylesheet={[
+            {
+                selector: 'node',
+                style: {
+                    width: 30,
+                    height: 30,
+                    backgroundColor: '#e35e94'
+                }
+            },
+            {
+                selector: 'edge',
+                style: {
+                    width: 1,
+                    lineColor: '#eee'
+                }
+            }
+        ]} style={{width: '80vw', height: '80vh'}} layout={this.state.layout}/>;
     }
 }
